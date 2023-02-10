f03dcb5a21768a95c5477b9c82eb702e93447ab8
wenzhe <mysteryven@gmail.com>
2023-02-10 16:41:13 +0800

    feat: add line info

--- src/chunk.rs
+++ src/chunk.rs
@@ -3,6 +3,7 @@ use crate::{opcode, value::Value};
 pub struct Chunk {
     code: Vec<u8>,
     constants: Vec<Value>,
+    lines: Vec<(usize, usize)>,
 }
 
 impl Chunk {
@@ -10,6 +11,7 @@ impl Chunk {
         Self {
             code: vec![],
             constants: vec![],
+            lines: vec![],
         }
     }
     pub fn code(&self) -> &Vec<u8> {
@@ -18,20 +20,21 @@ impl Chunk {
     pub fn constants(&self) -> &Vec<Value> {
         &self.constants
     }
-    pub fn write(&mut self, byte: u8) {
+    pub fn write(&mut self, byte: u8, line: usize) {
         self.code.push(byte);
+        self.save_line(line);
     }
     pub fn add_constant(&mut self, value: Value) -> usize {
         self.constants.push(value);
         self.constants.len() - 1
     }
-    pub fn add_u8(&mut self, value: u8) {
-        self.write(value)
+    pub fn add_u8(&mut self, value: u8, line: usize) {
+        self.write(value, line);
     }
-    pub fn add_u32(&mut self, value: u32) {
+    pub fn add_u32(&mut self, value: u32, line: usize) {
         let bytes = value.to_le_bytes();
         for i in 0..4 {
-            self.code.push(bytes[i]);
+            self.write(bytes[i], line)
         }
     }
 
@@ -42,6 +45,27 @@ impl Chunk {
     pub fn get_u32(&self, idx: usize) -> u32 {
         let bytes = unsafe { self.code.get_unchecked(idx..idx + 4) };
         u32::from_le_bytes(bytes.try_into().unwrap())
+    }
+
+    pub fn get_line(&self, index: usize) -> usize {
+        let mut total = 0;
+        for (line, count) in &self.lines {
+            total += count;
+            if total > index {
+                return *line;
+            }
+        }
+
+        0
+    }
+
+    pub fn save_line(&mut self, line: usize) {
+        match self.lines.last_mut() {
+            Some((current_line_num, count)) if *current_line_num == line => *count += 1,
+            _ => {
+                self.lines.push((line, 1));
+            }
+        }
     }
 }
 
@@ -56,56 +80,50 @@ mod test {
     #[test]
     fn disassemble_simple_ins() {
         let mut chunk = Chunk::new();
-        chunk.write(opcode::RETURN);
+        chunk.write(opcode::RETURN, 0);
         let debugger = Debugger::new(&chunk);
         let list = debugger.disassemble("test");
 
         assert_yaml_snapshot!(list, @r###"
         ---
         - "== test =="
-        - 00000000       OP_Return
+        - "00000000 0    OP_Return           "
         "###);
     }
 
     #[test]
     fn disassemble_unknown_ins() {
         let mut chunk = Chunk::new();
-        chunk.write(255);
-        chunk.write(opcode::RETURN);
+        chunk.write(255, 0);
+        chunk.write(opcode::RETURN, 1);
         let debugger = Debugger::new(&chunk);
         let list = debugger.disassemble("test");
         assert_yaml_snapshot!(list, @r###"
         ---
         - "== test =="
-        - 00000000    255(unknown)
-        - 00000001       OP_Return
+        - "00000000 0   255(unknown)        "
+        - "00000001 1    OP_Return           "
         "###)
-    }
-
-    fn create_opcodes(list: Vec<u8>, name: &str) -> Vec<String> {
-        let mut chunk = Chunk::new();
-        for op_code in list {
-            chunk.write(op_code);
-        }
-        let debugger = Debugger::new(&chunk);
-        debugger.disassemble(name)
     }
 
     #[test]
     fn disassemble_constant() {
         let mut chunk = Chunk::new();
-        chunk.write(opcode::CONSTANT);
+        chunk.write(opcode::RETURN, 0);
+        chunk.write(opcode::CONSTANT, 0);
         let idx = chunk.add_constant(Value::Number(1.2)) as u32;
-        chunk.add_u32(idx);
-        chunk.write(opcode::RETURN);
+        chunk.add_u32(idx, 0);
+        chunk.write(opcode::RETURN, 1);
         let debugger = Debugger::new(&chunk);
         let list = debugger.disassemble("test");
+        println!("{:?}", list);
 
         assert_yaml_snapshot!(list, @r###"
         ---
         - "== test =="
-        - 00000000     OP_Constant 00000000 Number(1.2)
-        - 00000005       OP_Return
+        - "00000000 0    OP_Return           "
+        - 00000001 |    OP_Constant          00000000 Number(1.2)
+        - "00000006 1    OP_Return           "
         "###);
     }
 }
--- src/debug.rs
+++ src/debug.rs
@@ -25,6 +25,11 @@ impl<'a> Debugger<'a> {
     pub fn disassemble_instruction(&self, result: &mut Vec<String>, offset: usize) -> usize {
         let mut line = String::new();
         line.push_str(format!("{:08}", offset).as_str());
+        if offset > 0 && self.chunk.get_line(offset) == self.chunk.get_line(offset - 1) {
+            line.push_str(format!(" {:<4}", "|").as_str());
+        } else {
+            line.push_str(format!(" {:<4}", self.chunk.get_line(offset)).as_str())
+        }
 
         let op_code = self.chunk.code().get(offset).unwrap().clone();
 
@@ -33,7 +38,7 @@ impl<'a> Debugger<'a> {
             opcode::CONSTANT => self.constant_instruction(&mut line, offset, "OP_Constant"),
             _ => {
                 // this is a unknown opcode
-                line.push_str(format!("{:>16}", format!("{}({})", op_code, "unknown")).as_str());
+                line.push_str(format!("{:<20}", format!("{}({})", op_code, "unknown")).as_str());
                 offset + 1
             }
         };
@@ -48,13 +53,13 @@ impl<'a> Debugger<'a> {
         offset: usize,
         name: &str,
     ) -> usize {
-        line.push_str(format!("{:>16}", name).as_str());
+        line.push_str(format!(" {:<20}", name).as_str());
 
         offset + 1
     }
 
     pub fn constant_instruction(&self, line: &mut String, offset: usize, name: &str) -> usize {
-        line.push_str(format!("{:>16}", name).as_str());
+        line.push_str(format!(" {:<20}", name).as_str());
         let constant = self.chunk.get_u32(offset + 1);
         let value = self.chunk.constants().get(constant as usize).unwrap();
         line.push_str(format!(" {:08} {:?}", constant, value).as_str());
